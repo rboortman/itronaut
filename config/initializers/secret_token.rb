8f22383199778e1974c5025268e7b7f1ed723f8e
Ron Boortman <ron@boortman.nl>
2012-01-09 13:06:01 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Itronaut::Application.config.secret_token = '12428c96df605da89c0714926c8f6310f6b5d33263c5527fecf25ad8b185a0cb8c86e27926af56fa7c9ef91f7125ed9ca2e6a3b381d70008fcea4d1d31871797'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Itronaut::Application.config.session_store :cookie_store, key: '_itronaut_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Itronaut::Application.config.session_store :active_record_store
